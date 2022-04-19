48fb7f4b72353e36356f44fbbee6b1c14ca94f34
vijayheartruler <101716101+vijayheartruler@users.noreply.github.com>
2022-04-19 10:47:34 +0530

    First Time Commit

---
+++ JenkinsFirstProject.java
@@ -0,0 +1,9 @@
+public class JenkinsFirstProject {
+
+ 	public static void main(String[] args) {
+  	// TODO Auto-generated method stub
+    	System.out.println("Hello Ruthvik");
+	System.out.println("Hello Thanvik");
+ }
+
+}
